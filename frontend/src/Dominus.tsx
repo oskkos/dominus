d755a3b21bd8fdbd93f16a0a458b4d19ae56a341
Oskari Kosonen <o.kosonen@gmail.com>
2020-04-04 23:58:34 +0300

    Just setting things up

---
+++ frontend/src/Dominus.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container} from "@material-ui/core";
+import {Alert} from "@material-ui/lab";
+
+
+export function Dominus(_props: {}): JSX.Element {
+    return <Container >
+        <Alert color={"error"}>This is Dominus!</Alert>
+    </Container>;
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Dominus} from "./Dominus";
 
+// eslint-disable-next-line functional/no-expression-statement
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Dominus/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +15,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+// eslint-disable-next-line functional/no-expression-statement
 serviceWorker.unregister();
