ceaba4e2d1a7cf7f5f269c52c26a9f97a82dc125
Oskari Kosonen <o.kosonen@gmail.com>
2020-04-09 16:52:01 +0300

    Initial commit for backend

---
+++ backend/server.ts
@@ -0,0 +1,28 @@
+import express = require("express");
+import bodyParser = require("body-parser");
+import cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (_req, res) => {
+  res.json({ message: "Welcome to dominus." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
