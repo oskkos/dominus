543f4765afc12583388eb2d01a6f2bc65e56e6b3
Oskari Kosonen <o.kosonen@gmail.com>
2020-04-12 12:28:49 +0300

    Small fixes for create-inital-tables

--- backend/migrations/00001_create-initial-tables.sql
+++ backend/migrations/00001_create-initial-tables.sql
@@ -1,41 +1,38 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE event_log
 (
     id SERIAL PRIMARY KEY,
-    target_uuid UUID NOT NULL,
+    target_model TEXT NOT NULL,
+    target_id INTEGER NOT NULL,
     event TEXT NOT NULL,
     data JSON NOT NULL,
     event_user TEXT NOT NULL DEFAULT CURRENT_USER,
-    event_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    event_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE roles
 (
     id SERIAL PRIMARY KEY,
-    uuid UUID NOT NULL DEFAULT uuid_generate_v1mc(),
     role TEXT UNIQUE
 );
 CREATE TABLE users
 (
     id SERIAL PRIMARY KEY,
-    uuid UUID NOT NULL DEFAULT uuid_generate_v1mc(),
     username TEXT NOT NULL,
     password TEXT NOT NULL,
     role INTEGER REFERENCES roles (id)
 );
 
-CREATE FUNCTION add_role(role_name text) RETURNS INTEGER
+CREATE OR REPLACE FUNCTION add_role(role_name text) RETURNS INTEGER
     LANGUAGE plpgsql
 AS
 $$
 DECLARE
     new_id INTEGER;
-    new_uuid UUID;
 BEGIN
-    INSERT INTO roles (role) VALUES (role_name) RETURNING id, uuid INTO new_id, new_uuid;
-    INSERT INTO event_log (target_uuid, event, data) VALUES(new_uuid, 'roleAdded', json_build_object('role_name', role_name));
+    INSERT INTO roles (role) VALUES (role_name) RETURNING id INTO new_id;
+    INSERT INTO event_log (target_model, target_id, event, data) VALUES('roles', new_id, 'roleAdded', json_build_object('role_name', role_name));
     RETURN new_id;
 END;
 $$;
