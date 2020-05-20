4211ec447466605a1e17958572e78c8dd24832fa
Oskari Kosonen <o.kosonen@gmail.com>
2020-05-20 23:40:42 +0300

    Open DB connection just once

--- backend/src/repositories/user.repository.ts
+++ backend/src/repositories/user.repository.ts
@@ -1,22 +1,20 @@
-import { createConnection } from 'typeorm';
+import { getConnection } from 'typeorm';
 import { UserWithCryptedPassword, User } from '../models/User';
 import { User as EntityUser } from '../entities/User';
 
 export async function openById(id: number): Promise<User> {
-  const connection = await createConnection();
+  const connection = getConnection();
   const userRepository = connection.getRepository(EntityUser);
   const user = await userRepository.findOne({ id });
-  connection.close();
   if (user) {
     return { id: user.id, username: user.username };
   }
   throw new Error(`User open failed with id: ${id}`);
 }
 export async function getByUserName(username: string): Promise<UserWithCryptedPassword | null> {
-  const connection = await createConnection();
+  const connection = getConnection();
   const userRepository = connection.getRepository(EntityUser);
   const user = await userRepository.findOne({ username });
-  connection.close();
   if (!user) {
     return null;
   }
@@ -28,9 +26,8 @@ export async function getByUserName(username: string): Promise<UserWithCryptedPa
 }
 
 export async function addUser(username: string, password: string, name: string): Promise<void> {
-  const connection = await createConnection();
+  const connection = getConnection();
   const userRepository = connection.getRepository(EntityUser);
   const user = new EntityUser(username, password, name);
   await userRepository.save(user);
-  connection.close();
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
-// import authController from './controllers/auth.routes';
-// import userController from './controllers/user.routes';
 
 require('dotenv').config();
 
 /* eslint-disable import/first */
+import { createConnection } from 'typeorm';
 import 'reflect-metadata';
 import swaggerUi from 'swagger-ui-express';
 import express, { Response, Request } from 'express';
@@ -14,40 +13,36 @@ import { decodeToken, getToken } from './middlewares/auth.jwt';
 import bodyParser = require('body-parser');
 import cors = require('cors');
 
-const logger = getLogger();
-const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-};
+createConnection().then(() => {
+  const logger = getLogger();
+  const app = express();
 
-app.use(cors(corsOptions));
+  const corsOptions = { origin: 'http://localhost:3000' };
+  app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
+  // parse requests of content-type - application/json
+  app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+  // parse requests of content-type - application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, _res, next) => {
-  const token = getToken(req);
-  configureLogger(token ? decodeToken(token) : null);
-  next();
-});
-// simple route
-app.get('/', (_req, res) => {
-  res.json({ message: 'Welcome to dominus.' });
-});
+  app.use((req, _res, next) => {
+    const token = getToken(req);
+    configureLogger(token ? decodeToken(token) : null);
+    next();
+  });
 
-app.use('/docs', swaggerUi.serve, async (_req: Request, res: Response) => res.send(
-  swaggerUi.generateHTML(await import('../swagger.json')),
-));
-RegisterRoutes(app);
+  app.use('/docs', swaggerUi.serve, async (_req: Request, res: Response) => res.send(
+    swaggerUi.generateHTML(await import('../swagger.json')),
+  ));
+  RegisterRoutes(app);
 
 
-// set port, listen for requests
-const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => {
-  configureLogger(null);
-  logger.info(`Server is running on port ${PORT}.`);
+  // set port, listen for requests
+  const PORT = process.env.PORT || 7000;
+  app.listen(PORT, () => {
+    configureLogger(null);
+    logger.info(`Server is running on port ${PORT}.`);
+  });
 });
