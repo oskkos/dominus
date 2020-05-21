2eeb601c57fc9cbf177792adb83832054fc32ae2
Oskari Kosonen <o.kosonen@gmail.com>
2020-05-21 13:06:04 +0300

    Custom logger for TypeORM

---
+++ backend/src/entities/TypeORMLogger.ts
@@ -0,0 +1,38 @@
+import { Logger, QueryRunner } from 'typeorm';
+import { getLogger } from '../middlewares/logger';
+
+const logger = getLogger('TypeORM');
+export class TypeORMLogger implements Logger {
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  log(level: 'log' | 'info' | 'warn', message: unknown, _queryRunner?: QueryRunner): void {
+    if (level === 'log') {
+      logger.debug(message);
+    } else {
+      logger[level](message);
+    }
+  }
+
+  logMigration(message: string, _queryRunner?: QueryRunner): void {
+    logger.info('migration', message);
+  }
+
+  logQuery(query: string, parameters?: unknown[], _queryRunner?: QueryRunner): void {
+    logger.trace(`${query}${parameters ? `. Params: [${parameters.join(', ')}]` : ''}`);
+  }
+
+  logQueryError(
+    error: string, query: string, parameters?: unknown[], _queryRunner?: QueryRunner,
+  ): void {
+    logger.error(`Received error ${error} while performing query: ${query}`, parameters ?? []);
+  }
+
+  logQuerySlow(
+    time: number, query: string, parameters?: unknown[], _queryRunner?: QueryRunner,
+  ): void {
+    logger.warn(`Slow (${time} ms) query: ${query}: `, parameters ?? []);
+  }
+
+  logSchemaBuild(message: string, _queryRunner?: QueryRunner): void {
+    logger.debug('Schema build.', message);
+  }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,48 +1,52 @@
-
 require('dotenv').config();
 
 /* eslint-disable import/first */
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 import 'reflect-metadata';
 import swaggerUi from 'swagger-ui-express';
 import express, { Response, Request } from 'express';
 import { RegisterRoutes } from '../routes';
+import { TypeORMLogger } from './entities/TypeORMLogger';
 import { configureLogger, getLogger } from './middlewares/logger';
 import { decodeToken, getToken } from './middlewares/auth.jwt';
 
 import bodyParser = require('body-parser');
 import cors = require('cors');
 
+configureLogger(null);
+getConnectionOptions().then((connectionOptions) => {
+  createConnection({
+    ...connectionOptions,
+    logger: new TypeORMLogger(),
+  }).then(() => {
+    const logger = getLogger();
+    const app = express();
 
-createConnection().then(() => {
-  const logger = getLogger();
-  const app = express();
+    const corsOptions = { origin: 'http://localhost:3000' };
+    app.use(cors(corsOptions));
 
-  const corsOptions = { origin: 'http://localhost:3000' };
-  app.use(cors(corsOptions));
+    // parse requests of content-type - application/json
+    app.use(bodyParser.json());
 
-  // parse requests of content-type - application/json
-  app.use(bodyParser.json());
+    // parse requests of content-type - application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-  // parse requests of content-type - application/x-www-form-urlencoded
-  app.use(bodyParser.urlencoded({ extended: true }));
+    app.use((req, _res, next) => {
+      const token = getToken(req);
+      configureLogger(token ? decodeToken(token) : null);
+      next();
+    });
 
-  app.use((req, _res, next) => {
-    const token = getToken(req);
-    configureLogger(token ? decodeToken(token) : null);
-    next();
-  });
-
-  app.use('/docs', swaggerUi.serve, async (_req: Request, res: Response) => res.send(
-    swaggerUi.generateHTML(await import('../swagger.json')),
-  ));
-  RegisterRoutes(app);
+    app.use('/docs', swaggerUi.serve, async (_req: Request, res: Response) => res.send(
+      swaggerUi.generateHTML(await import('../swagger.json')),
+    ));
+    RegisterRoutes(app);
 
 
-  // set port, listen for requests
-  const PORT = process.env.PORT || 7000;
-  app.listen(PORT, () => {
-    configureLogger(null);
-    logger.info(`Server is running on port ${PORT}.`);
+    // set port, listen for requests
+    const PORT = process.env.PORT || 7000;
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}.`);
+    });
   });
 });
